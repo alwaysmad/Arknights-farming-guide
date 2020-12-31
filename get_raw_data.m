c165421d14eef58a187c6a09311fae7ff5dfc6bc
alwaysmad <tchertovskihpavel@yandex.ru>
2020-12-31 22:17:02 +0700

    fixed t1 rock value

--- get_raw_data.m
+++ get_raw_data.m
@@ -9,7 +9,7 @@ resp = send(r,uri);
 disp(resp.StatusCode); % show status code
 tmp = jsondecode(resp.Body.string());
 raw_item_data = tmp.items;
-%% get craft data
+%% get craft data (EN)
 url = "https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/en_US/gamedata/excel/building_data.json";
 uri = URI(url);
 fprintf("Getting craft data from %s\n", url)
@@ -38,7 +38,7 @@ resp = send(r,uri);
 disp(resp.StatusCode); % show status code
 tmp = jsondecode(resp.Body.string());
 raw_event_stage_data = tmp.stages;
-%% get drop rate data (EN statistics for main stages)
+%% get drop rate data (CN statistics for main stages)
 %url = "https://penguin-stats.io/PenguinStats/api/v2/result/matrix?server=US";
 url = "https://penguin-stats.io/PenguinStats/api/v2/result/matrix?server=CN";
 uri = URI(url);
